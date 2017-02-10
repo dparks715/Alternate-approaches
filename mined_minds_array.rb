54eb22556567a46788cf1e4ca2513ffae25fa141
Dan <dparks715@yahoo.com>
2017-02-09 19:52:03 -0500

    Used splat operator to fill array with 1-100

--- mined_minds_array.rb
+++ mined_minds_array.rb
@@ -1,3 +1,9 @@
+#Create a function that fills and array with 1 to 100.
+#Replace numbers divisible by 5 and 3 with 'mined minds'
+#Replace numbers divisible by 5 with 'minds'
+#Replace numbers divisible by 3 with 'mined'
+
 def mmarray()
-	nums = []
+	#Using splat operator to create an array filled with range 1-100
+	nums = *(1..100)
 end
