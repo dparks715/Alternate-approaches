e6fcbf15b213195c44a0ef02e364925f0735175c
Dan <dparks715@yahoo.com>
2017-02-10 04:18:12 -0500

    Trying .map function to create pairs

--- Pairing/pairing.rb
+++ Pairing/pairing.rb
@@ -1,4 +1,14 @@
+#Takes an array of items (names, letters, etc.) and pairs them up
+#Places them in a new array after pairing.
+
 def pairs(letters)
-	letters
+	#Trying out the .map with index method.
+	#Attempting to push odd index values into the previous value (index - 1) to create pairs.
+	#Just adding value if not odd, but I think this will give too many pairs.
+	letters.map.with_index { |value, index| if index % 2 == 1
+												letters[index - 1] << value
+											else 
+												value
+											end }
 	
 end
