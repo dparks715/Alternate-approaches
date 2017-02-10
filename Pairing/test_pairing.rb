6baa6dbdf4ac733040d17cc4d33d6476d908d4e6
Dan <dparks715@yahoo.com>
2017-02-10 03:54:18 -0500

    Test file started

---
+++ Pairing/test_pairing.rb
@@ -0,0 +1,11 @@
+require 'minitest\autorun'
+require_relative 'pairing.rb'
+
+class TestParingFunction < Minitest::Test
+	#First test verifies test function works.
+	def test_true_is_true
+		assert_equal(true, true)
+		
+	end
+
+end
