f359dae50fd293c5a52f8f8d27d9aff4023f1fcb
Dan <dparks715@yahoo.com>
2017-02-09 19:50:20 -0500

    Comments for goal of function, new test for array length

--- test_mined_minds_array.rb
+++ test_mined_minds_array.rb
@@ -10,6 +10,10 @@ class TestMinedMindsArray < Minitest::Test
 	def test_function_produces_array
 		results = mmarray()
 		assert_equal(Array, results.class)
-		
+	end
+
+	def test_length_of_array
+		results = mmarray()
+		assert_equal(100, results.length)
 	end
 end
