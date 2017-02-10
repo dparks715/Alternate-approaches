373214b81ab36174b942505beb68cec4c6f2f00d
Dan <dparks715@yahoo.com>
2017-02-09 20:17:57 -0500

    Function complete.  Minor comments and formatting.

--- mined_minds_array.rb
+++ mined_minds_array.rb
@@ -10,12 +10,15 @@ def mmarray()
 	#Will use .map method to replace our numbers
 	#Mapping (iterating over) each value in the array.  Uses val as a variable for the value at each index position.
 	#Using an IF statement inside the block to replace numbers divisible by 5 and 3 first.
-	#Replaces divisible by 5 next.
+	#Replaces divisible by 5 next, then divisble by 3
+	#If not divisible by 5 or 3, just uses the current value.
 	nums.map { |val| if val % 5 == 0 && val % 3 == 0
 						'mined minds'
-					elsif val % 5 == 0
+					elsif
+						val % 5 == 0
 						'minds'
-					elsif val % 3 == 0
+					elsif
+						val % 3 == 0
 						'mined'
 					else
 						val
