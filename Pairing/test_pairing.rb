88cdfa80488da7a1f467668d071a67e8ccdb6239
Dan <dparks715@yahoo.com>
2017-02-10 03:57:49 -0500

    First test for returned class.  Start function.

--- Pairing/test_pairing.rb
+++ Pairing/test_pairing.rb
@@ -4,8 +4,12 @@ require_relative 'pairing.rb'
 class TestParingFunction < Minitest::Test
 	#First test verifies test function works.
 	def test_true_is_true
-		assert_equal(true, true)
-		
+		assert_equal(true, true)		
+	end
+
+	def test_array_class
+		results = pairs(['A', 'B'])
+		assert_equal(Array, results.class)
 	end
 
 end
