8851503ae5b8de9cf89b0298d5cba4d458a3e88e
Dan <dparks715@yahoo.com>
2017-02-09 20:02:51 -0500

    Changed function to replace div by 5 and 3, regular numbers fail now

--- mined_minds_array.rb
+++ mined_minds_array.rb
@@ -6,4 +6,12 @@
 def mmarray()
 	#Using splat operator to create an array filled with range 1-100
 	nums = *(1..100)
+
+	#Will use .map method to replace our numbers
+	#Mapping (iterating over) each value in the array.  Uses val as a variable for the value at each index position.
+	#Using an IF statement inside the block to replace numbers divisible by 5 and 3.
+	nums.map { |val| if val % 5 == 0 && val % 3 == 0
+						'mined minds'
+						end }
+
 end
