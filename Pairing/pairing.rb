ab9f0ffdd49a8ac2971d584fe4e8c7a7d6c4846e
Daniel Parks <dparks715@yahoo.com>
2017-02-11 18:48:38 -0500

    Dropped .map, switched to more appropriate method .zip

--- Pairing/pairing.rb
+++ Pairing/pairing.rb
@@ -1,14 +1,16 @@
 #Takes an array of items (names, letters, etc.) and pairs them up
 #Places them in a new array after pairing.
 
+#Abandoned .map, could probably make it work in an unnecessarily long way.
+#Using .zip found in ruby docs instead.  Need to split array in half to use it.
 def pairs(letters)
-	#Trying out the .map with index method.
-	#Attempting to push odd index values into the previous value (index - 1) to create pairs.
-	#Just adding value if not odd, but I think this will give too many pairs.
-	letters.map.with_index { |value, index| if index % 2 == 1
-												letters[index - 1] << value
-											else 
-												value
-											end }
-	
+	#shuffle bang! shuffles letters in place permanently
+	letters.shuffle!
+	#Slicing array into 2 halves and assigning to one and two, while converting to array
+	#.round places the uneven element into the first array (one)
+	one,two = letters.each_slice((letters.size/2.0).round).to_a
+	#Found applicable method in ruby docs for Array
+	#Converts arguments to arrays and merges corresponding elements
+	#Syntax below will take one[0] and merge with two[0], so on through the index
+	pairs = one.zip(two)
 end
