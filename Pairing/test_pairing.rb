fa771ce9998dfcb239c11a9adff2851c31a29e44
Daniel Parks <dparks715@yahoo.com>
2017-02-11 18:56:03 -0500

    Added several more tests for even and odd, all pass

--- Pairing/test_pairing.rb
+++ Pairing/test_pairing.rb
@@ -27,4 +27,24 @@ class TestParingFunction < Minitest::Test
 		assert_equal(2, results.count)
 	end
 
+	def test_seven_letters_three_pairs
+		results = pairs(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
+		assert_equal(3, results.count)
+	end
+
+	def test_eight_letters_four_pairs
+		results = pairs(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'])
+		assert_equal(4, results.count)
+	end
+
+	def test_ten_letters_five_pairs
+		results = pairs(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'])
+		assert_equal(5, results.count)
+	end
+
+	def test_eleven_letters_five_pairs
+		results = pairs(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'])
+		assert_equal(5, results.count)
+	end
+
 end
