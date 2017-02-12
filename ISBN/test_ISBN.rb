058e4909c0ea7e640a551c6811896976fc0647df
Daniel Parks <dparks715@yahoo.com>
2017-02-11 20:10:45 -0500

    Added tests for true and false

--- ISBN/test_ISBN.rb
+++ ISBN/test_ISBN.rb
@@ -7,9 +7,24 @@ class TestISBN < Minitest::Test
 		assert_equal(1, 1)
 	end
 
-	def test_detect_invalid_chars
+	def test_detect_invalid_chars_a
 		results = check_chars('1234a5')
 		assert_equal(false, results)	
+	end
+
+	def test_none_invalid_true
+		results = check_chars('1 2 3-4-5')
+		assert_equal(true, results)	
+	end
+
+	def test_detect_invalid_chars_symbol
+		results = check_chars('$12345')
+		assert_equal(false, results)	
+	end
+
+	def test_only_numbers_true
+		results = check_chars('1234567890')
+		assert_equal(true, results)	
 	end
 
 end
