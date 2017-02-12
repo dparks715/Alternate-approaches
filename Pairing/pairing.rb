58ed50e150863ac886a56310e266ce66f33ae408
Daniel Parks <dparks715@yahoo.com>
2017-02-11 19:00:16 -0500

    Function complete, more comments for clarity

--- Pairing/pairing.rb
+++ Pairing/pairing.rb
@@ -13,10 +13,14 @@ def pairs(letters)
 	#Converts arguments to arrays and merges corresponding elements
 	#Syntax below will take one[0] and merge with two[0], so on through the index
 	pairs = one.zip(two)
-
+	#If statement to catch inputs with uneven numbered arrays.
+	#If the arrays sliced in half aren't the same length, we know there is an extra letter.
+	#Pushes the first element in the last 'inner' array [-1][0], to the first element of the 'outer' array.
+	#Deletes the last element
 	if one.length != two.length
 		pairs[0] << pairs[-1][0]
 		pairs.delete_at(-1)
 	end
+	#executes pairs
 		pairs
 end
